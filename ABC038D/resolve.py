5a5cc683c254639fc488cc81e40160f4870c6cd0
Sho Taguchi <taguchi.sho@mail.canon>
2020-06-10 12:59:21 +0900

    aaa

---
+++ ABC038D/resolve.py
@@ -0,0 +1,13 @@
+def resolve():
+    '''
+    code here
+    '''
+
+    N = int(input())
+    boxes = [[int(item) for item in input().split()] for _ in range(N)]
+
+    
+
+
+if __name__ == "__main__":
+    resolve()
